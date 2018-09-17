d0be5a2c43eb5417effc925a2f96585cfe43ce32
Cheryl K <cherylkelson@icloud.com>
2018-09-17 15:44:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,5 @@
-
+require `pry`
+def pry deli_counter.rb
 line_array = []
 
 def line(katz_deli)
@@ -6,17 +7,16 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each_with_index(1) do | name, index|
-    line_array.push(#{index}, #{name}")
-        end
-        message = "The line is currently : #{index}"
-    puts # {message}
+    line_array.push("#{name}, #{index}")
+      end 
+        puts "The line is currently: #{line_array.join (" ")}
   end
 end
 
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  puts "Welcome, "#{name}. You are number #{katz_deli.length} in line."
 end
 
 
